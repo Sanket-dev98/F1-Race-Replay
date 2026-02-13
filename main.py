cd6eece43d3e94e88b43c34941aa9f4e4b64c047
Sanket-dev98 <magarsanket98@gmail.com>
2026-02-13 16:34:56 +0530

    Enable the cache

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import fastf1
+import os
 
-print("F1 Race Replay Project Started!")
+# Enable the cache
+def enable_cache():
+    cache_dir = ".fastf1-cache"
+
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+
+    fastf1.Cache.enable_cache(cache_dir)
+    print(f"Fastf1 cache enabled \nenabled at: {cache_dir}")
+
+def load_session(year, round_number, session_type):
+    session = fastf1.get_session(year , round_number , session_type)
+    session.load()
+    return session
+
+
+
